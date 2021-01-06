42374c3817bc319184b258531865b021e28dc882
“polo” <polo@inipro.net>
2021-01-06 14:00:48 +0900

    update in v2020.2

--- src/hls/filter.cpp
+++ src/hls/filter.cpp
@@ -1,46 +1,32 @@
 #include "filter.h"
 
-
-void filter(hls::stream<ap_axiu<32,1,1,1> > &src_axi,
-		hls::stream<ap_axiu<32,1,1,1> > &dst_axi)
+void filter(hls::stream<ap_axiu<24,1,1,1>> &src_axi,
+			hls::stream<ap_axiu<24,1,1,1>> &dst_axi)
 {
 #pragma HLS INTERFACE axis port=dst_axi
 #pragma HLS INTERFACE axis port=src_axi
 #pragma HLS DATAFLOW
 #pragma HLS INTERFACE ap_ctrl_none port=return
 
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC3> img1(MAX_HEIGHT, MAX_WIDTH);
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC3> img2(MAX_HEIGHT, MAX_WIDTH);
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC3> img1_1(MAX_HEIGHT, MAX_WIDTH);
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC1> img1_1_Y(MAX_HEIGHT, MAX_WIDTH);
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC1> img1_1_U(MAX_HEIGHT, MAX_WIDTH);
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC1> img1_1_V(MAX_HEIGHT, MAX_WIDTH);
-	hls::Mat<MAX_HEIGHT, MAX_WIDTH, HLS_10UC1> img1_2(MAX_HEIGHT, MAX_WIDTH);
+	xf::cv::Mat<XF_8UC3,MAX_HEIGHT, MAX_WIDTH, NPC> img1(MAX_HEIGHT, MAX_WIDTH);
+	xf::cv::Mat<XF_8UC1,MAX_HEIGHT, MAX_WIDTH, NPC> img2(MAX_HEIGHT, MAX_WIDTH);
+	xf::cv::Mat<XF_8UC1,MAX_HEIGHT, MAX_WIDTH, NPC> img3(MAX_HEIGHT, MAX_WIDTH);
+	xf::cv::Mat<XF_8UC3,MAX_HEIGHT, MAX_WIDTH, NPC> img4(MAX_HEIGHT, MAX_WIDTH);
 
-	const int k_dummy[3][3] =
-	{ { -1, -1, -1 },
-	  { -1,  8, -1 },
-	  { -1, -1, -1 }
+	short int kernel[9] = {
+			-1, -1, -1,
+			-1,  8, -1,
+			-1, -1, -1
 	};
 
-	hls::Window<3,3,int> kernel;
-	hls::Point_<int> anchor;
+	xf::cv::AXIvideo2xfMat(src_axi, img1);
 
-	for (int i=0; i<3; i++)
-		for (int j=0; j<3; j++)
-			kernel.val[i][j] = k_dummy[i][j];
+	xf::cv::rgb2gray<XF_8UC3, XF_8UC1, MAX_HEIGHT, MAX_WIDTH, NPC>(img1, img2);
 
-	anchor.x = -1;
-	anchor.y = -1;
+	xf::cv::filter2D<XF_BORDER_CONSTANT, 3, 3, XF_8UC1, XF_8UC1, MAX_HEIGHT, MAX_WIDTH, NPC>(img2, img3, kernel, 0);
 
-	hls::AXIvideo2Mat(src_axi, img1);
+	xf::cv::gray2rgb<XF_8UC1, XF_8UC3, MAX_HEIGHT, MAX_WIDTH, NPC>(img3, img4);
 
-	hls::CvtColor<HLS_BGR2YCrCb>(img1, img1_1);
-	hls::Split(img1_1, img1_1_Y, img1_1_U, img1_1_V);
-	hls::Consume(img1_1_U);
-	hls::Consume(img1_1_V);
-	hls::Filter2D(img1_1_Y, img1_2, kernel, anchor);
-	hls::CvtColor<HLS_GRAY2RGB>(img1_2, img2);
-
-	hls::Mat2AXIvideo(img2, dst_axi);
+	xf::cv::xfMat2AXIvideo(img4, dst_axi);
 }
+
